a3931e480c73ffe80c185c02135f70db09e9bc59
chris <cvera8873@gmail.com>
2024-09-23 23:11:08 -0500

    updated webpack

---
+++ Develop/client/webpack.config.js
@@ -0,0 +1,58 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const WebpackPwaManifest = require('webpack-pwa-manifest');
+const path = require('path');
+const { InjectManifest } = require('workbox-webpack-plugin');
+
+// TODO: Add and configure workbox plugins for a service worker and manifest file.
+// TODO: Add CSS loaders and babel to webpack.
+
+module.exports = () => {
+  return {
+    mode: 'development',
+    entry: {
+      main: './src/js/index.js',
+      install: './src/js/install.js'
+    },
+    output: {
+      filename: '[name].bundle.js',
+      path: path.resolve(__dirname, 'dist'),
+    },
+    plugins: [
+      new HtmlWebpackPlugin({
+        template: './index.html',
+        title: 'Just Another Text Editor',
+      }),
+
+      new InjectManifest({
+        swSrc: './src-sw.js', 
+        swDest: 'src-sw.js',
+      }),
+
+      new WebpackPwaManifest({
+        fingerprints: false,
+        inject: true,
+        name: 'Just Another Text Editor',
+        short_name: 'JATE',
+        description: 'Best Text Editor Ever!',
+        background_color: '#ffffff',
+        theme_color: '#317EFB',
+        start_url: '/',
+        publicPath: '/',
+        icons: [
+          {
+            src: path.resolve('src/images/logo.png'),
+            sizes: [96, 128, 192, 256, 384, 512], // Multiple icon sizes for different platforms
+            destination: path.join('assets', 'icons'),
+          },
+        ],
+      }),
+    ],
+
+
+    module: {
+      rules: [
+        
+      ],
+    },
+  };
+};
